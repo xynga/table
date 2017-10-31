d2cd3f9639014220098dd6e3119e396baf9957a6
jaronson13 <aronson_jesse@wheatoncollege.edu>
2017-10-31 15:18:23 -0400

    add column headers class amd col-toggle icon

--- tables.module.ts
+++ tables.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from "@angular/core";
 import { TableComponent } from "./table/table.component";
 import {CommonModule} from "@angular/common";
+import {GeneralModule} from "xynga-general"
 
 @NgModule({
-  declarations: [
-    TableComponent,
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    TableComponent,
-  ],
-  providers: []
+    declarations: [
+        TableComponent,
+    ],
+    imports: [
+        CommonModule,
+        GeneralModule
+    ],
+    exports: [
+        TableComponent,
+    ],
+    providers: []
 })
 export class TablesModule {}
