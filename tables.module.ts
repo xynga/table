058459b3d6492333bde2891e7918e03e88d2bf3d
jaronson13 <aronson_jesse@wheatoncollege.edu>
2017-11-01 11:04:53 -0400

    add ColumnHeader class to tables.module

--- tables.module.ts
+++ tables.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { TableComponent } from "./table/table.component";
+import { ColumnHeader } from "./table/table.component";
 import {CommonModule} from "@angular/common";
 import {GeneralModule} from "xynga-general"
 
 @NgModule({
     declarations: [
         TableComponent,
+        ColumnHeader
     ],
     imports: [
         CommonModule,
@@ -13,6 +15,7 @@ import {GeneralModule} from "xynga-general"
     ],
     exports: [
         TableComponent,
+        ColumnHeader
     ],
     providers: []
 })
