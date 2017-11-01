f1f99cf9a02bb6394e8b7e617db5c458118b9020
jaronson13 <aronson_jesse@wheatoncollege.edu>
2017-11-01 11:15:46 -0400

    remove ColumnHeader from module (only needed in index.ts)

--- tables.module.ts
+++ tables.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from "@angular/core";
 import { TableComponent } from "./table/table.component";
-import { ColumnHeader } from "./table/table.component";
 import {CommonModule} from "@angular/common";
 import {GeneralModule} from "xynga-general"
 
 @NgModule({
     declarations: [
-        TableComponent,
-        ColumnHeader
+        TableComponent
     ],
     imports: [
         CommonModule,
         GeneralModule
     ],
     exports: [
-        TableComponent,
-        ColumnHeader
+        TableComponent
     ],
     providers: []
 })
