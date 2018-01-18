6114905c1c756cde07af776db9378c7e2a22ce5e
alaroche <(Blue22810)>
2018-01-18 11:50:53 -0500

    -Attempting to get Karma working to test this component (creating/modifying Karma code in app.component.spec.ts) -Received vague error message and did some research to find there's sometimes an issue with newer angular CLI versions -ng test -sm=false seems to produce a more explicit error -Need to find a way to create and fill variables associated with getElementsByTagName (the bodyChildren and headerChildren seem to be required in order to test if the component is even being created) these variables are found in src/table.component.ts (outside of testing directory)

--- testing/src/app/app.component.spec.ts
+++ testing/src/app/app.component.spec.ts
@@ -9,23 +9,23 @@ describe('Table-Component', () => {
       declarations: [
         AppComponent,
         TableComponent,
-        ColumnHeader
       ],
     }).compileComponents();
   }));
   it('should create the table', async(() => {
     const fixture = TestBed.createComponent(TableComponent);
     const table = fixture.debugElement.componentInstance;
-    table.columnHeaders = [new ColumnHeader('sagsadg',false),
-      new ColumnHeader('sagsadg',false),
-      new ColumnHeader('sagsadg',false)];
-    fixture.debugElement.nativeElement.innerHTML = `<tr *ngFor="let entry of tableData">
-    <td>{{entry.firstName}}</td>
-    <td>{{entry.lastName}}</td>
-    <td>{{entry.emailAddress}}</td>
-    <td>{{entry.lifeGoal}}</td>
-  </tr>`
     table.maxHeight = 300;
+    table.columnHeaders = [new ColumnHeader('First Name', false),
+      new ColumnHeader('Last Name', false),
+      new ColumnHeader('Email Address', false),
+      new ColumnHeader('Life Goal')];
+    table.tableData= `<tr cghfghsdh>
+    <td>sfassagsag</td>
+    <td>asfgasg</td>
+    <td>asfgasg</td>
+    <td>Fafasdfasdgasg</td>
+  </tr>`;
     fixture.detectChanges();
     expect(TableComponent).toBeTruthy();
   }));
