e44b254d9834bc85a94e4053dd9ee23f3b8b3c8e
alaroche <(Blue22810)>
2018-03-16 13:25:41 -0400

    end to end testing successful.

--- testing/e2e/app.e2e-spec.ts
+++ testing/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { by, element} from "protractor";
 
 describe('testing App', () => {
   let page: AppPage;
@@ -7,8 +8,36 @@ describe('testing App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Should have original order before click event on First Name', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    let inOrderList = element(by.css('.table-data')).all(by.tagName('td'));
+    expect(inOrderList.get(0).getText()).toBe('Peter');
+    // page.getFirstName().click();
+  });
+  it('Should alphabetically order First Names after first click event on First Name', () => {
+    page.navigateTo();
+    page.getFirstName().click();
+    let inOrderList = element(by.css('.table-data')).all(by.tagName('td'));
+    expect(inOrderList.get(4).getText()).toBe('Steven');
+  });
+  it('Should reverse alphabetically order First Names after second click event on First Name', () => {
+    page.navigateTo();
+    page.getFirstName().click();
+    page.getFirstName().click();
+    let inOrderList = element(by.css('.table-data')).all(by.tagName('td'));
+    expect(inOrderList.get(0).getText()).toBe('Wade');
+  });
+  it('Should alphabetically order Email Addresses after first click event on Email Address', () => {
+    page.navigateTo();
+    page.getEmailAddress().click();
+    let inOrderList = element(by.css('.table-data')).all(by.tagName('td'));
+    expect(inOrderList.get(2).getText()).toBe('BreakThe4thWall@Gotcha.com');
+  });
+  it('Should reverse alphabetically order Email Addresses after first click event on Email Address', () => {
+    page.navigateTo();
+    page.getEmailAddress().click();
+    page.getEmailAddress().click();
+    let inOrderList = element(by.css('.table-data')).all(by.tagName('td'));
+    expect(inOrderList.get(2).getText()).toBe('tstark@starkindustries.net');
   });
 });
